d32c37d3672bbaa883441b784d5b9179b185272d
Bogdan Catangiu <bogdan.catangiu@gmail.com>
2018-11-26 22:09:22 +0000

    Add move up/down functionality

--- game_functions.py
+++ game_functions.py
@@ -8,6 +8,10 @@ def check_keydown_events(event, ship):
 		ship.moving_right = True
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = True
+	elif event.key == pygame.K_UP:
+		ship.moving_up =True
+	elif event.key == pygame.K_DOWN:
+		ship.moving_down = True
 
 def check_keyup_events(event, ship):
 	"""Respond to key releases."""
@@ -15,18 +19,20 @@ def check_keyup_events(event, ship):
 		ship.moving_right = False
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left = False
-
-
+	elif event.key == pygame.K_UP:
+		ship.moving_up = False
+	elif event.key == pygame.K_DOWN:
+		ship.moving_down = False
 
 def check_events(ship):
 	"""Respond to keypresses and mouse events."""
 	for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-			elif event.type == pygame.KEYDOWN:
-				check_keydown_events(event, ship)
-			elif event.type == pygame.KEYUP:
-				check_keyup_events(event, ship)
+		if event.type == pygame.QUIT:
+			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			check_keydown_events(event, ship)
+		elif event.type == pygame.KEYUP:
+			check_keyup_events(event, ship)
 				
 def update_screen(ai_settings, screen, ship):
 	"""Update images on the screen and flip to the new screen."""
