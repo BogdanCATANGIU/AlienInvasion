5d5419d6a2a68fbf359977510773bd33330fcfef
Bogdan Catangiu <bogdan.catangiu@gmail.com>
2018-11-27 11:59:35 +0000

    Add a fleet of aliens

--- ship.py
+++ ship.py
@@ -13,7 +13,8 @@ class Ship():
 		self.screen_rect = screen.get_rect()
 
 		#Start each new ship at the center of the screen.
-		self.rect.center = self.screen_rect.center
+		self.rect.centerx = self.screen_rect.centerx
+		self.rect.bottom = self.screen_rect.bottom
 
 		# Store a decimal value for ship's center.
 		self.centerx = float(self.rect.centerx)
